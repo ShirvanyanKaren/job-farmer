74041ab92669d0cf6697393d0ac33b12f995a666
ShirvanyanKaren <kshirvanyan2000@gmail.com>
2024-01-27 14:29:06 -0800

    functionality: added menu to main function, job experience/levels, and limit

--- linkedin.py
+++ linkedin.py
@@ -7,18 +7,23 @@ from selenium.webdriver.support import expected_conditions as ec
 from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
 from fs_operations import write_to_file, read_from_file
 import time
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+phone_number = config['General_Info']['number']
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
-
-user_prompts = True
+user_prompts = False
 
 
 def login():
-    email_phone = ''
-    password = ''
     try:
+        email_phone = config['Linkedin']['username']
+        password = config['Linkedin']['password']
         if user_prompts:
             email_phone = input("Enter the email address or phone number to your LinkedIn account: ")
             password = input("Enter the password for your LinkedIn account: ")
@@ -48,7 +53,9 @@ def search_jobs(**kwargs):
         time.sleep(5)
         search_bar = driver.find_element(By.XPATH,
                                          "/html/body/div[4]/header/div/div/div/div[2]/div[2]/div/div/input[1]")
+        time.sleep(1)
         search_bar.send_keys(kwargs["search_key"])
+        time.sleep(1)
         search_bar.send_keys(Keys.ENTER)
         time.sleep(5)
         if kwargs["remote"]:
@@ -63,6 +70,16 @@ def search_jobs(**kwargs):
             driver.find_element(By.XPATH,
                                 '/html/body/div[4]/div[3]/div[4]/section/div/section/div/div/div/ul/li[8]/div/button').click()
             time.sleep(1)
+        if kwargs["experience_level"] is not None:
+            driver.find_element(By.XPATH,'//*[@id="searchFilter_experience"]').click()
+            time.sleep(1)
+            for experience_level in kwargs["experience_level"]:
+                driver.find_element(By.XPATH,
+                                    f'//span[text()="{experience_level}"]').click()
+                time.sleep(1)
+            driver.find_element(By.XPATH,
+                                '/html/body/div[4]/div[3]/div[4]/section/div/section/div/div/div/ul/li[6]/div/div/div/div[1]/div/form/fieldset/div[2]/button[2]/span').click()
+            time.sleep(1)
         print(f'Searching for jobs using the keyword {kwargs["search_key"]}.')
         return True
     except NoSuchElementException as e:
@@ -71,7 +88,7 @@ def search_jobs(**kwargs):
         print("An error occurred:", e)
 
 
-def get_job_ids():
+def get_job_ids(jobs_added, jobs_num, page_count):
     job_ids = []
     try:
         job_listings = WebDriverWait(driver, 10).until(
@@ -82,12 +99,18 @@ def get_job_ids():
             job_id = job_item.get_attribute('data-occludable-job-id')
             if job_id is not None:
                 job_ids.append(int(job_id))
+                jobs_added += 1
+            if jobs_num != "max" and jobs_added >= jobs_num:
+                break
     except NoSuchElementException as e:
         print("Some elements were not found:", e)
     except Exception as e:
         print("An error occurred:", e)
-    write_to_file(job_ids, 'job_ids.txt')
-    return job_ids
+    finally:
+        write_to_file(job_ids, 'job_ids.txt')
+        print(f"Went through {page_count} page(s)")
+        page_count += 1
+    return job_ids, jobs_added, page_count
 
 
 def parse_url():
@@ -116,15 +139,19 @@ def get_next_url():
     else:
         raise EndOfJobs()
 
-
-def save_jobs():
+def save_jobs(**kwargs):
+    jobs_num = kwargs["number_of_jobs"]
+    jobs_added = 0
+    page_count = 1
     running = True
     while running:
         try:
             element = WebDriverWait(driver, 10).until(ec.presence_of_element_located(
                 (By.CLASS_NAME, 'scaffold-layout__list-container')))
             if element is not None:
-                get_job_ids()
+                if jobs_num != "max" and jobs_added >= jobs_num:
+                    break
+                _, jobs_added, page_count = get_job_ids(jobs_added, jobs_num, page_count)
                 time.sleep(5)
                 next_url = get_next_url()
                 time.sleep(5)
@@ -134,3 +161,4 @@ def save_jobs():
                 running = False
         except EndOfJobs:
             running = False
+
